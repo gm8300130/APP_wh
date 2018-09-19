bfc05eabe931007b4a74dd74304cdbe8148ffaf3
yutian(小寶) <gm8300130@gmail.com>
2018-09-19 18:22:23 +0800

    etc_zabbix_access_status.sh

---
+++ access_status.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Envir=`cat /etc/zabbix/zabbix_agentd.conf | grep Hostname= | grep -v "#" | awk 'BEGIN {FS="_"}{print $2}'`
+
+if [ "$1" == "3xx" ] ; then
+	SC="3[0-9][0-9]"
+elif [ "$1" == "4xx" ] ; then
+	SC="4[0-9][0-9]"
+elif [ "$1" == "5xx" ] ; then
+	SC="5[0-9][0-9]"
+else
+	SC="$1"
+fi
+
+StatusCode=$SC
+#PortNU=$2
+
+#function APP_Status() {
+	cat /opt/logs/nginx/*.access.log | grep `date -d '1 minute ago' '+%d/%b/%Y:%H:%M'` | grep " status=$StatusCode" | wc -l
+#}
+
+#function BE_Status() {
+#        cat /opt/logs/nginx/*.access.log | grep `date -d '1 minute ago' '+%d/%b/%Y:%H:%M'` | grep " status=$StatusCode" | wc -l
+#}
+
+#function FE_Status() {
+#        cat /opt/logs/nginx/*.access.log | grep `date -d '1 minute ago' '+%d/%b/%Y:%H:%M'` | grep " status=$StatusCode" | wc -l
+#}
+
+#function PAY_Status() {
+#        cat /opt/logs/nginx/*.access.log | grep `date -d '1 minute ago' '+%d/%b/%Y:%H:%M'` | grep " status=$StatusCode" | wc -l
+#}
+#$Envir"_Status"
